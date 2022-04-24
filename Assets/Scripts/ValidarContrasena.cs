f330a93fe7969645fdc1df6e02b2ef04f50ddb0c
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2022-04-24 20:03:26 -0300

    Todo

---
+++ Assets/Scripts/ValidarContrasena.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasena : MonoBehaviour
+{
+    string contrasenaCorrecta;
+    string contrasena;
+    public Text Ingreso;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+    
+    void Start()
+    {
+        contrasenaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasena()
+    {
+        contrasena = Ingreso.text;
+        if(contrasenaCorrecta == contrasena)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido.";
+            Debug.Log("Bienvenido.");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contrasena Incorrecta.";
+            Debug.Log("Contrasena Incorrecta.");
+        }
+    }
+}
